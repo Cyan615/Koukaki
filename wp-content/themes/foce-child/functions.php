226fd6652ad19a4c98ce5c058acc192db568525f
N.Maurice <cirilaii.nm@gmail.com>
2024-01-30 10:19:20 +0100

    etape 1

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -2,6 +2,14 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    // Chargement du theme.css pour nos personnalisations
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', 
+    array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
+
+    wp_enqueue_script( 'foce-child', get_stylesheet_directory_uri() . '/js/animation.js', array(), _S_VERSION, true );
+
+	
 }
 
 // Get customizer options form parent theme

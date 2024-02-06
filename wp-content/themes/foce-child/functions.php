591ede3767c957c06f7bf8a5f00186a6072655de
N.Maurice <cirilaii.nm@gmail.com>
2024-02-06 18:04:39 +0100

    carrousel section personnage en coverflow

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -7,9 +7,11 @@ function theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', 
     array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
 
+// chargement de Swiper via cdn
+    wp_enqueue_style('swiper-style', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', );
+    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), _S_VERSION, true);
+// chargement du fichier JS
     wp_enqueue_script( 'foce-child', get_stylesheet_directory_uri() . '/js/animation.js', array(), _S_VERSION, true );
-
-	
 }
 
 // Get customizer options form parent theme
--- wp-content/themes/foce-child/js/animation.js
+++ wp-content/themes/foce-child/js/animation.js
@@ -1,4 +1,6 @@
-console.log("ok ");
+console.log('go');
+
+// Animation des titres de sections au scroll
 const titles = document.querySelectorAll('.fadeTitle') 
 const observer = new IntersectionObserver(entries => {
     console.log(entries);
@@ -14,4 +16,39 @@ const observer = new IntersectionObserver(entries => {
 titles.forEach(title => {
     observer.observe(title)
 })
+
+
+//effet parallaxe au scroll sur le logo
+const parallax = document.querySelector('#site-logo');
+
+window.addEventListener('scroll', () => {
+    parallax.style.top = window.scrollY / 2 + "px";
+})
+
+
+// Carrousel des personnages avec effet cover flow: utilisation de Swiper js
+const swiper = new Swiper(".main-character", {
+    effect:"coverflow",
+    centeredSlider: true,
+    // boucle
+    loop: true,
+    coverflowEffect:{
+        rotate: 60,
+        stretch: 0,
+        // depth:100,
+        slideShadows:false,
+        modifier:1,
+    },
+    slidesPerView: '3',
+    // pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // navigation
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
 
